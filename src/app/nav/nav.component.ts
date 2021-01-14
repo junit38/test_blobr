7b90b9b5c4cec0e5c61c33773390b4b2c6393de1
Junit38 <Junit38@gmail.com>
2021-01-14 18:01:17 +0100

    Finish nav css, add graph css, and add statistics css

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -19,18 +19,58 @@ export class NavComponent {
 
   year = '2020';
 
-  months = [
-    'December',
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September'
+  months = [{
+      month: 'November',
+      year: 2019
+    },
+    {
+      month: 'December',
+      year: 2019
+    },
+    {
+      month: 'January',
+      year: 2019
+    },
+    {
+      month: 'February',
+      year: 2020
+    },
+    {
+      month: 'March',
+      year: 2020
+    },
+    {
+      month: 'April',
+      year: 2020
+    },
+    {
+      month: 'May',
+      year: 2020
+    },
+    {
+      month: 'June',
+      year: 2020
+    },
+    {
+      month: 'July',
+      year: 2020
+    },
+    {
+      month: 'August',
+      year: 2020
+    },
+    {
+      month: 'September',
+      year: 2020
+    }
   ];
+
+  monthsLength = this.months.length;
+
+  selectedMonth = {
+    month: 'September',
+    year: 2020
+  }
 
   constructor() { }
 
