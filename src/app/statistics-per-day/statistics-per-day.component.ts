3558d8cefe6621441103b6c2b94247a1dfd200c3
Junit38 <Junit38@gmail.com>
2021-01-15 15:03:34 +0100

    add css statistic-per-day; Multiple fixes

--- src/app/statistics-per-day/statistics-per-day.component.ts
+++ src/app/statistics-per-day/statistics-per-day.component.ts
@@ -15,12 +15,52 @@ export class StatisticsPerDayComponent {
 
   statistics = [
     {
-      date: new Date(),
+      date: new Date('September 1 2020'),
       statistics: [
         {
           header: 'Revenue',
           value: 2,
           transactions: 1
+        },
+        {
+          header: 'Refunds',
+          value: 0,
+          transactions: 0
+        },
+        {
+          header: 'Chargebacks',
+          value: 0,
+          transactions: 0
+        }
+      ]
+    },
+    {
+      date: new Date('September 2 2020'),
+      statistics: [
+        {
+          header: 'Revenue',
+          value: 1,
+          transactions: 1
+        },
+        {
+          header: 'Refunds',
+          value: 0,
+          transactions: 0
+        },
+        {
+          header: 'Chargebacks',
+          value: 0,
+          transactions: 0
+        }
+      ]
+    },
+    {
+      date: new Date('September 3 2020'),
+      statistics: [
+        {
+          header: 'Revenue',
+          value: 3,
+          transactions: 2
         },
         {
           header: 'Refunds',
