4eaf0c1ddf2cebfdd4434e630f196437b3c6720c
Junit38 <Junit38@gmail.com>
2021-01-15 16:24:57 +0100

    add popup

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  private popupOpen = false;
+  private wasInside = false;
+
+  @HostListener('click')
+  clickInside() {
+    this.wasInside = true;
+  }
+
+  @HostListener('document:click')
+  clickout() {
+    if (!this.wasInside) {
+      this.popupOpen = false;
+    }
+    this.wasInside = false;
+  }
 
   constructor() { }
 
+  togglePopup() {
+    this.popupOpen = !this.popupOpen;
+  }
 }
