f1ebe866559f0ff543181bff4c7bc168bb8cd9aa
Junit38 <Junit38@gmail.com>
2021-01-15 12:37:11 +0100

    Finish statistics css; Add statistic-per-day css

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -10,23 +10,23 @@ export class StatisticsComponent {
   statistics = [
     {
       name: 'Revenue',
-      value: '5',
+      value: 5.00,
       transactions: 4,
-      lastYearValue: 0,
+      lastYearValue: 0.00,
       lastYearTransactions: 0
     },
     {
       name: 'Refunds',
-      value: '0',
+      value: 0.00,
       transactions: 0,
       lastYearValue: 0,
       lastYearTransactions: 0
     },
     {
       name: 'Chargebacks',
-      value: '0',
+      value: 0.00,
       transactions: 0,
-      lastYearValue: 0,
+      lastYearValue: 0.00,
       lastYearTransactions: 0
     }
   ]
